fa65898594e8907dde6620d89961104d72de9696
Timur Gazizulin <t4ynyyy@gmail.com>
2023-12-04 23:41:24 +0300

    Initial Commit

---
+++ UpTodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpTodo
+//
+//  Created by Timur Gazizulin on 4.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
